8ba3e78f8d5243abff30b3a758746ef5f0e19e8e
Erin Khoo <866947+erinkhoo@users.noreply.github.com>
2022-11-17 14:49:48 +0000

    fixed bug in return of element (added a = that was not needed)

--- Web_Scraping/scraping.py
+++ Web_Scraping/scraping.py
@@ -1,23 +1,23 @@
 #Scraping data from a webpage using Selenium
 from selenium import webdriver
 
-def get_driver():
+def get_drvr():
   #Chrome webdrivers to make scraping simpler
   options = webdriver.ChromeOptions()
   options.add_argument("disable-infobars")
-  options.add_argument("start-maximised")
+  options.add_argument("start-maximized")
   options.add_argument("disable-dev-shm-usage")
   options.add_argument("no-sandbox")
-  options.add_experimental_option("excludeSwitches",["enable-automation"])
-  options.add_experimental_option("disable-blink-feature=AutomationControlled")
+  options.add_experimental_option("excludeSwitches", ["enable-automation"])
+  options.add_argument("disable-blink-feature=AutomationControlled")
 
   driver = webdriver.Chrome(options)
   driver.get("https://automated.pythonanywhere.com")
   return driver
 
 def main():
-  scrape = get_driver()
+  scrape = get_drvr()
   element = scrape.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-  return = element.text
+  return element
 
 print(main())
