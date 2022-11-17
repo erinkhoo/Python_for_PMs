0eac3aa2b6ff89ffb100f583a698d219f734539d
Erin Khoo <866947+erinkhoo@users.noreply.github.com>
2022-11-17 16:49:12 +0000

    working code in main.py for time delay number scrape

--- Web_Scraping/scraping.py
+++ Web_Scraping/scraping.py
@@ -1,5 +1,7 @@
 #Scraping data from a webpage using Selenium
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 def get_drvr():
   #Chrome webdrivers to make scraping simpler
@@ -11,13 +13,16 @@ def get_drvr():
   options.add_experimental_option("excludeSwitches", ["enable-automation"])
   options.add_argument("disable-blink-feature=AutomationControlled")
 
-  driver = webdriver.Chrome(options)
+  driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
   driver.get("https://automated.pythonanywhere.com")
   return driver
 
+
 def main():
   scrape = get_drvr()
-  element = scrape.find_element(by="xpath", value="/html/body/div[1]/div/h1[1]")
-  return element
+  element = scrape.find_element(by="xpath",
+                                value="/html/body/div[1]/div/h1[1]")
+  return element.text
+
 
 print(main())
--- get_drvr.py
+++ get_drvr.py
@@ -1,8 +1,3 @@
-#Scraping dynamic data from a webpage using Selenium
-from selenium import webdriver
-import time
-
-
 def get_drvr():
   #Chrome webdrivers to make scraping simpler
   options = webdriver.ChromeOptions()
@@ -16,21 +11,3 @@ def get_drvr():
   driver = webdriver.Chrome(options=options)
   driver.get("http://automated.pythonanywhere.com")
   return driver
-
-
-def clean_text(text):
-  """Extract only the temp from text"""
-  output = int(text.split(": ")[1])
-  return output
-
-
-def main():
-  scrape = get_drvr()
-  time.sleep(2.22)
-  element = scrape.find_element(by="xpath",
-                                value="/html/body/div[1]/div/h1[2]")
-  return clean_text(element.text)
-
-
-yo = main()
-print(f'Temp is: {yo}')
